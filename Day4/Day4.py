0c78c91f0c1fc128dcf1cc96e23f4130f682858f
Katherine Turk <kbt@terminusdatascience.com>
2022-12-06 22:55:34 -0600

    part 1 is still wrong

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -1,27 +1,27 @@
 # Read in the input
 pairs = []
 while True:
-  try:
-    line = input()
-  except EOFError:
-    break
-  pairs.append(line)
+    try:
+        line = input()
+    except EOFError:
+        break
+    pairs.append(line)
 
 # Parse the pairs into ranges
 ranges = []
 for pair in pairs:
-  for range_str in pair.split(','):
-    ranges.append(list(map(int, range_str.split('-'))))
+    for range_str in pair.split(','):
+        ranges.append(list(map(int, range_str.split('-'))))
 print(ranges)
-
 # Count the number of pairs where one range fully contains the other
 count = 0
-for i in range(len(ranges)):
-  for j in range(i+1, len(ranges)):
-    if ranges[i][0] <= ranges[j][0] and ranges[i][1] >= ranges[j][1]:
-      count += 1
-    elif ranges[j][0] <= ranges[i][0] and ranges[j][1] >= ranges[i][1]:
-      count += 1
+for i in range(0, len(ranges), 2):
+    for j in range(i+1, len(ranges), 2):
+        if ranges[i][0] <= ranges[j][0] and ranges[i][1] >= ranges[j][1]:
+            print(ranges[i],ranges[j],count)
+            count += 1
+        elif ranges[j][0] <= ranges[i][0] and ranges[j][1] >= ranges[i][1]:
+            count += 1
 
 # Print the result
 print(count)
