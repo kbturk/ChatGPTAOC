236abc01a2c0c611b1e11ef68e98055cf382313d
Katherine Turk <kbt@terminusdatascience.com>
2022-12-07 14:03:51 -0600

    fixed the \n issue, but it still needs to fix the range issue

--- Day4/Day4_pt2.py
+++ Day4/Day4_pt2.py
@@ -1,5 +1,8 @@
 input = "2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8"
 
+# Replace newline characters with commas
+input = input.replace("\n", ",")
+
 # Split the input into individual pairs
 pairs = input.split(",")
 
