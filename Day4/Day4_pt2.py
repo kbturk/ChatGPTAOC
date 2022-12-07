721a0e499a685eeb0ede389082a888e3aa346581
Katherine Turk <kbt@terminusdatascience.com>
2022-12-07 15:40:45 -0600

    'mom'(me) fixed the code. ChatGPT had a heck of a time parsing the input string, even when I gave it an example of the text and resulting string

--- Day4/Day4_pt2.py
+++ Day4/Day4_pt2.py
@@ -3,15 +3,24 @@ from typing import List
 
 
 def main(args: List[str]):
-    input_string = sys.stdin.read().replace("\n","")
-    #Parse the input into a list of tuples of the form (start, end)
+    input_string = sys.stdin.read()
+    print(input_string)
+    
+
+    # Parse the input into a list of tuples of the form (start, end)
+    input_string = input_string.replace('\n', ',')
+    input_string = input_string[:-1]
     pairs = [(int(pair.split('-')[0]), int(pair.split('-')[1])) for pair in input_string.split(',')]
-    print(pairs)
+
     # Keep track of the number of overlapping pairs
     overlapping_pairs = 0
 
     # Iterate over all pairs of pairs
     for i in range(0, len(pairs), 2):
+        # Check if there is another pair available
+        if i + 1 >= len(pairs):
+            break
+            
         pair1 = pairs[i]
         pair2 = pairs[i + 1]
 
@@ -21,6 +30,7 @@ def main(args: List[str]):
 
     # Print the result
     print(overlapping_pairs)
+
     return 1
 
 if __name__ == '__main__':
