c512cf9cafe447baa59c0e0d927bd886f050edb6
Katherine Turk <kbt@terminusdatascience.com>
2022-12-09 23:10:24 -0600

    Day 10 try 2

--- Day10/Day10.py
+++ Day10/Day10.py
@@ -7,6 +7,9 @@ X = 1
 # Initialize the cycle number to 0
 cycle = 0
 
+# Initialize the signal strength at each point in time to 0
+signal_strength = [0, 0, 0, 0, 0, 0]
+
 # Execute the program
 while cycle < len(program):
   # Increment the cycle number
@@ -18,9 +21,11 @@ while cycle < len(program):
   # If the instruction is "addx", increment the X register by the specified value
   if instruction.startswith('addx'):
     X += int(instruction.split()[1])
+  
+  # If the current cycle is the 20th, 60th, 100th, 140th, 180th, or 220th cycle,
+  # update the signal strength at that point in time
+  if cycle in [20, 60, 100, 140, 180, 220]:
+    signal_strength[cycle // 40] = cycle * X
 
-# Compute the signal strength of the CPU
-signal_strength = cycle * X
-
-# Print the signal strength
-print(signal_strength)
+# Print the signal strength at each point in time
+print(sum(signal_strength))
